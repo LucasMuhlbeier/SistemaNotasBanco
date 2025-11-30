130b9edcd5532cc1e0f21f0f847a6b05d4edb485
LucasMuhlbeier <166084418+LucasMuhlbeier@users.noreply.github.com>
2025-11-30 00:23:42 -0300

    Update LancamentoNotasDTO.java

--- src/main/java/com/example/sistemanotas/dto/LancamentoNotasDTO.java
+++ src/main/java/com/example/sistemanotas/dto/LancamentoNotasDTO.java
@@ -10,11 +10,11 @@ import java.math.BigDecimal;
 @Data
 public class LancamentoNotasDTO {
 
-    // O ID da Matrícula (AlunoDisciplina) é o que está sendo atualizado
+
     @NotNull(message = "O ID da matrícula é obrigatório")
     private Long id;
 
-    // As notas são opcionais, pois podem ser lançadas em momentos diferentes
+
     @DecimalMin(value = "0.0", message = "A nota deve ser no mínimo 0.0")
     @DecimalMax(value = "10.0", message = "A nota deve ser no máximo 10.0")
     private BigDecimal nota1Bimestre;
@@ -25,5 +25,5 @@ public class LancamentoNotasDTO {
 
     @DecimalMin(value = "0.0", message = "A nota deve ser no mínimo 0.0")
     @DecimalMax(value = "10.0", message = "A nota deve ser no máximo 10.0")
-    private BigDecimal notaFinal; // Nota de recuperação/exame
+    private BigDecimal notaFinal;
 }
