3abb554887cf5177cd77526a19a021e643e3d4be
LucasMuhlbeier <166084418+LucasMuhlbeier@users.noreply.github.com>
2025-11-30 00:21:02 -0300

    Update ProfessorController.java

--- src/main/java/com/example/sistemanotas/controller/ProfessorController.java
+++ src/main/java/com/example/sistemanotas/controller/ProfessorController.java
@@ -13,10 +13,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-// Configuração de CORS para o frontend Angular
+
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/professor") // Base URL para todos os endpoints de Professor
+@RequestMapping("/api/professor")
 @Tag(name = "Professor", description = "Endpoints para gerenciamento de Professores")
 public class ProfessorController {
 
@@ -26,9 +26,9 @@ public class ProfessorController {
         this.professorService = professorService;
     }
 
-    // --- Endpoints de Professor ---
 
-    // Cadastrar professor: POST api/professor
+
+
     @Operation(summary = "Cadastrar um novo professor", description = "Recebe os dados do professor e verifica a unicidade de CPF/Matrícula.")
     @PostMapping
     public ResponseEntity<ProfessorResponseDTO> cadastrarProfessor(@RequestBody @Valid ProfessorCadastroDTO dto) {
@@ -37,30 +37,30 @@ public class ProfessorController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    // Atualizar professor: PUT api/professor
+
     @Operation(summary = "Atualizar dados de um professor", description = "Atualiza o nome do professor pelo ID.")
     @PutMapping
     public ResponseEntity<ProfessorResponseDTO> atualizarProfessor(@RequestBody @Valid ProfessorAtualizacaoDTO dto) {
         ProfessorResponseDTO response = professorService.atualizarProfessor(dto);
-        // Retorna 200 OK
+
         return ResponseEntity.ok(response);
     }
 
-    // Buscar professor por id: GET api/professor/{id}
+
     @Operation(summary = "Buscar professor por ID", description = "Retorna os dados cadastrais de um professor específico.")
     @GetMapping("/{id}")
     public ResponseEntity<ProfessorResponseDTO> buscarProfessorPorId(@PathVariable Long id) {
         ProfessorResponseDTO response = professorService.buscarProfessorPorId(id);
-        // Retorna 200 OK
+
         return ResponseEntity.ok(response);
     }
 
-    // Listar todos os professores: GET api/professor/todos
+
     @Operation(summary = "Listar todos os professores", description = "Retorna uma lista de todos os professores cadastrados.")
     @GetMapping("/todos")
     public ResponseEntity<List<ProfessorResponseDTO>> listarTodosProfessores() {
         List<ProfessorResponseDTO> response = professorService.listarTodosProfessores();
-        // Retorna 200 OK
+
         return ResponseEntity.ok(response);
     }
 }
