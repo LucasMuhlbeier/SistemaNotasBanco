fc68ad1008870dc1486dbd75a0629f109acbf1b1
LucasMuhlbeier <166084418+LucasMuhlbeier@users.noreply.github.com>
2025-11-30 00:23:08 -0300

    Update LancamentoBimestralDTO.java

--- src/main/java/com/example/sistemanotas/dto/LancamentoBimestralDTO.java
+++ src/main/java/com/example/sistemanotas/dto/LancamentoBimestralDTO.java
@@ -2,19 +2,16 @@ package com.example.sistemanotas.dto;
 
 import java.math.BigDecimal;
 
-/**
- * DTO para lançamento de nota e faltas em um bimestre específico.
- */
+
 public class LancamentoBimestralDTO {
 
-    // Nota do bimestre (1 ou 2)
+
     private BigDecimal nota;
 
-    // Faltas registradas no bimestre (este campo é mantido para aderir ao enunciado,
-    // mas o cálculo de faltas será sempre feito pelo total em AulasDadasPresencas).
+
     private Integer faltas;
 
-    // Construtores, Getters e Setters
+
 
     public BigDecimal getNota() {
         return nota;
