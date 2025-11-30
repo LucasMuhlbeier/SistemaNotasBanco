f0943a7506eb8c58b74335f26b718c19481d18c2
LucasMuhlbeier <166084418+LucasMuhlbeier@users.noreply.github.com>
2025-11-30 00:25:36 -0300

    Update Aluno.java

--- src/main/java/com/example/sistemanotas/model/Aluno.java
+++ src/main/java/com/example/sistemanotas/model/Aluno.java
@@ -7,9 +7,9 @@ import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "aluno")
-@Data // Gera Getters, Setters, toString, equals e hashCode (Lombok)
-@NoArgsConstructor // Construtor sem argumentos (JPA e Lombok)
-@AllArgsConstructor // Construtor com todos os argumentos (Lombok)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Aluno {
 
     @Id
@@ -23,7 +23,7 @@ public class Aluno {
     private String cpf;
 
     @Column(nullable = false, unique = true)
-    private String ra; // Registro Acadêmico
+    private String ra;
 
     @Column(name = "ano_ingresso")
     private Integer anoIngresso;
@@ -31,10 +31,5 @@ public class Aluno {
     @Column(name = "periodo_atual")
     private Integer periodoAtual;
 
-    // Relacionamento com AlunoDisciplina (para o futuro)
-    // Uma vez que implementaremos AlunoDisciplina (matrícula), o relacionamento seria:
-    /*
-    @OneToMany(mappedBy = "aluno", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<AlunoDisciplina> matriculas = new ArrayList<>();
-    */
+
 }
