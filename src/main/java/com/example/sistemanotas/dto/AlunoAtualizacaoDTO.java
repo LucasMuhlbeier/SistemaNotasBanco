55897a2f457d5cfd8f97d286685af42d65fba70f
LucasMuhlbeier <166084418+LucasMuhlbeier@users.noreply.github.com>
2025-11-30 00:21:17 -0300

    Update AlunoAtualizacaoDTO.java

--- src/main/java/com/example/sistemanotas/dto/AlunoAtualizacaoDTO.java
+++ src/main/java/com/example/sistemanotas/dto/AlunoAtualizacaoDTO.java
@@ -7,10 +7,9 @@ import lombok.Data;
 public class AlunoAtualizacaoDTO {
 
     @NotNull(message = "O ID do aluno é obrigatório para atualização")
-    private Long id; // Crucial para o PUT
+    private Long id;
 
-    // Os outros campos podem ser os mesmos do AlunoCadastroDTO, mas neste caso,
-    // vamos permitir a atualização de nome e período, mantendo CPF e RA como únicos (não alteráveis por aqui, idealmente)
+
     private String nome;
     private Integer periodoAtual;
 }
