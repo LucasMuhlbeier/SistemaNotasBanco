48202d980c4ab3ea565f7fb9b4df297d24672222
LucasMuhlbeier <166084418+LucasMuhlbeier@users.noreply.github.com>
2025-11-30 00:28:19 -0300

    Update ProfessorRepository.java

--- src/main/java/com/example/sistemanotas/repository/ProfessorRepository.java
+++ src/main/java/com/example/sistemanotas/repository/ProfessorRepository.java
@@ -1,6 +1,6 @@
-package com.example.sistemanotas.repository; // Pacote ajustado
+package com.example.sistemanotas.repository; 
 
-import com.example.sistemanotas.model.Professor; // Import ajustado
+import com.example.sistemanotas.model.Professor; 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
