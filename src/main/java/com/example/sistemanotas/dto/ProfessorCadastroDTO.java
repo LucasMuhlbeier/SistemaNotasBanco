c0c169f3e5e13734194f5cd2458588e7b06987b5
LucasMuhlbeier <166084418+LucasMuhlbeier@users.noreply.github.com>
2025-11-30 00:24:28 -0300

    Update ProfessorCadastroDTO.java

--- src/main/java/com/example/sistemanotas/dto/ProfessorCadastroDTO.java
+++ src/main/java/com/example/sistemanotas/dto/ProfessorCadastroDTO.java
@@ -7,11 +7,11 @@ import lombok.Data;
 public class ProfessorCadastroDTO {
 
     @NotBlank(message = "O número de matrícula é obrigatório")
-    private String matricula; // Única e obrigatória
+    private String matricula; 
 
     @NotBlank(message = "O nome é obrigatório")
     private String nome;
 
     @NotBlank(message = "O CPF é obrigatório")
-    private String cpf; // Único e obrigatório
+    private String cpf; 
 }
