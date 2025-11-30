397c3da4834935223cbc29d19ce19acdea18103e
LucasMuhlbeier <166084418+LucasMuhlbeier@users.noreply.github.com>
2025-11-30 00:22:11 -0300

    Update DisciplinaAtualizacaoDTO.java

--- src/main/java/com/example/sistemanotas/dto/DisciplinaAtualizacaoDTO.java
+++ src/main/java/com/example/sistemanotas/dto/DisciplinaAtualizacaoDTO.java
@@ -7,10 +7,10 @@ import lombok.Data;
 public class DisciplinaAtualizacaoDTO {
 
     @NotNull(message = "O ID da disciplina é obrigatório para atualização")
-    private Long id; // ID da Disciplina a ser atualizada
+    private Long id;
 
-    private Long professorId; // Novo professor (opcional)
+    private Long professorId;
     private String descricao;
     private String ementa;
-    // O código da disciplina (codigo) geralmente não é alterado
+
 }
