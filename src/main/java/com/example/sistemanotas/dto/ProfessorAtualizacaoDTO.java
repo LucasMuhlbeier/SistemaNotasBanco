0b0f1f854a492e6f1c27853486a7ec8cce3e2364
LucasMuhlbeier <166084418+LucasMuhlbeier@users.noreply.github.com>
2025-11-30 00:24:18 -0300

    Update ProfessorAtualizacaoDTO.java

--- src/main/java/com/example/sistemanotas/dto/ProfessorAtualizacaoDTO.java
+++ src/main/java/com/example/sistemanotas/dto/ProfessorAtualizacaoDTO.java
@@ -7,8 +7,7 @@ import lombok.Data;
 public class ProfessorAtualizacaoDTO {
 
     @NotNull(message = "O ID do professor é obrigatório para atualização")
-    private Long id; // Crucial para identificar quem será atualizado
-
+    private Long id; 
     private String nome;
-    // Matrícula e CPF não são incluídos aqui por segurança, geralmente são imutáveis após o cadastro
+  
 }
